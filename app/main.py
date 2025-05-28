6849fdf79f96ebc38434379ee2c5426272ebaa18
CraftsManShip001 <seoj080829@gmail.com>
2025-05-28 00:26:43 -0700

    fix :: middleware 수정

--- app/main.py
+++ app/main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
 from app.api import endpoints
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(endpoints.router)
